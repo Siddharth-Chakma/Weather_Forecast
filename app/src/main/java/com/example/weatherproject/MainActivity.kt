9aeaeab403186cff7c45ed422f96379593aadf15
Chungus20 <65286878+Chungus20@users.noreply.github.com>
2022-05-10 15:50:58 +0600

    update_sid

--- app/src/main/java/com/example/weatherproject/MainActivity.kt
+++ app/src/main/java/com/example/weatherproject/MainActivity.kt
@@ -1,11 +1,94 @@
 package com.example.weatherproject
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.weatherproject.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var activityMainBinding: ActivityMainBinding
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+        supportActionBar?.hide()
+
+        fusedLocationProviderClient= LocationServices.getFusedLocationProviderClient(this)
+
+        getCurrentLocation();
     }
+
+    private fun getCurrentLocation()
+    {
+        if(checkPermissions())
+        {
+            if(isLocationEnabled())
+            {
+                if(ActivityCompat.checkSelfPermission(
+                        this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        this,
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ){
+                    requestPermission()
+                    return
+                }
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener(this){ task->
+                    val location: Location?=task.result
+                    if(location==null)
+                    {
+                        Toast.makeText(this,"Null Received", Toast.LENGTH_SHORT).show()
+                    }
+                    else
+                    {
+                        /*fetch weather here*/
+                        fetchCurrentLocationWeather(location.latitude.toString(),location.longitude.toString())
+                    }
+
+                }
+            }
+            else
+            {
+                Toast.makeText(this,"Turn on Location",Toast.LENGTH_SHORT).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+
+
+            }
+        }
+        else
+        {
+            requestPermission()
+        }
+    }
+
+    private fun fetchCurrentLocationWeather(latitude: String,longitude:String) {
+
+    }
+
+
+    private fun isLocationEnabled():Boolean{...}
+
+    private fun requestPermission() {...}
+
+    companion object{...}
+
+    private fun checkPermissions():Boolean
+    {...}
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {...}
 }
